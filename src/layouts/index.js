d463319a7c9932318df926a3f63968b9dc513a46
Javier Martinez <Javier.Martinez@akqa.com>
2018-07-07 15:53:51 +1200

    Start scaffolding scss files

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,28 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import '../styles/main.scss'
+import '../styles/main.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Hello world!"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
@@ -32,4 +23,4 @@ TemplateWrapper.propTypes = {
   children: PropTypes.func,
 }
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1 className="u-h1">Hello world!</h1>
+    <h2 className="u-h2">My name is Javier Martinez, and I’m a Web Developer. <br/>I specialize in UX/UI development.</h2>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )

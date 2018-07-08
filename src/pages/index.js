df28bb1b7f44a372132c6c2a8d4d95ad63e79179
Javier Martinez <Javier.Martinez@akqa.com>
2018-07-08 15:27:45 +1200

    Some homepage styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import laptop from '../assets/img/laptop.svg';
 
 const IndexPage = () => (
-  <div className="l-full-width">
-    <div className="grid grid--center grid--full">
+  <div className="jm-page l-full-height  l-align-vertical">
+    <div className="l-full-width">
+      <div className="grid grid--center grid--middle">
 
-        <div className="grid__item one-third">
-        1
+          <div className="grid__item one-third">
+          <img src={laptop} alt="" />
 
-        </div>
-        <div className="grid__item two-thirds">
-            <h1 className="u-h1">Hello world!</h1>
-            <h2 className="u-h2">My name is Javier Martinez, and I’m a Web Developer. <br/>I specialize in UX/UI development.</h2>
-            <Link to="/page-2/">Go to page 2</Link>
-        </div>
+          </div>
+          <div className="grid__item two-thirds">
+              <div className="u-margin-left">
+                <h1 className="u-h1 u-color-white">Hello World!</h1>
+                <h2 className="u-h2 u-color-white">My name is Javier Martinez, and I’m a Web Developer. <br/>I specialize in UX/UI development.</h2>
+                <Link to="/page-2/">Go to page 2</Link>
+              </div>
+          </div>
+      </div>
     </div>
   </div>
 )
